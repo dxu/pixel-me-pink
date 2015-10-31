a1552d8c6f4fdcc6b08cdfe67f32c7f73a14ae83
David <xudavidb@gmail.com>
2015-10-31 13:37:15 -0700

    test

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,32 @@
-console.log('hi')
+let canvas
+function createPixel(ctx, x = 0, y = 0, color = {}) {
+  const pixel = ctx.createImageData(1, 1)
+  pixel.data[0] = color.r || 0
+  pixel.data[1] = color.g || 0
+  pixel.data[2] = color.g || 0
+  pixel.data[3] = color.a || 100
+  ctx.putImageData(pixel, x, y)
+}
+(function() {
+  $(function() {
+    let context, size, pixel
+    window.canvas = canvas = $('canvas').get(0)
+    context = canvas.getContext('2d')
+    canvas.width = 50
+    canvas.height = 50
+    size = Math.min(document.body.clientWidth, document.body.clientHeight)
+    canvas.style.width = size
+    canvas.style.height = size
+    context.imageSmoothingEnabled = false
+    context.mozImageSmoothingEnabled = false
+    // context.webkitImageSmoothingEnabled = false
+    context.msImageSmoothingEnabled = false
+
+    createPixel(context, 1, 1, {
+      r: 0,
+      g: 0,
+      b: 0,
+      a: 255
+    })
+  })
+})()
