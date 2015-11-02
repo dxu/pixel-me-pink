6b89d8a99b9703c6bd2c233e51488e0ef79b2db9
David <xudavidb@gmail.com>
2015-11-01 19:53:03 -0800

    add actions

---
+++ src/scripts/actions.js
@@ -0,0 +1,26 @@
+
+/*
+ * ADD_PIXEL:
+ * data: {
+ *   x: x-coordinate
+ *   y: y-coordinate
+ *   r: red
+ *   g: green
+ *   b: blue
+ *   a: alpha channel from 0-255
+ * }
+ */
+export var ADD_PIXEL = 'ADD_PIXEL'
+
+/*
+ * ADD_PIXEL:
+ * data: {
+ *   x: x-coordinate
+ *   y: y-coordinate
+ *   r: red
+ *   g: green
+ *   b: blue
+ *   a: alpha channel from 0-255
+ * }
+ */
+export var REMOVE_PIXEL = 'REMOVE_PIXEL'
